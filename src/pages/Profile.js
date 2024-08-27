00e73c8631c8878d99bd5c1d0825db5bbd73ed8b
Dmitriy <khotindima@gmail.com>
2024-08-27 13:58:09 +0300

    Fixed deploy

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,7 +6,6 @@ const Profile = () => {
     return (
         <Layout>
             <h1>{user?.username} - Писюн</h1>
-            <img src={user?.photo_url} alt="user photo" />
             {JSON.stringify(user)}
 
         </Layout>
