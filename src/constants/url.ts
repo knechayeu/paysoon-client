ae36973f9cd71d6cc14e7f908e78754a8e5251c4
Dmitriy Khotin <khotindima@gmail.com>
2025-02-20 13:56:59 +0300

    update env variables

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,4 +1,4 @@
-const BACKEND_HOST: string = process.env.BACKEND_HOST || '';
+const BACKEND_HOST: string = process.env.RAILWAY_PUBLIC_DOMAIN || '';
 
 export const BACKEND_URL = {
     GetRoom: `${BACKEND_HOST}/room`,
